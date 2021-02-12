e622874fc0480192709e0649fe9ef1dbf3828966
adrielc <adrielcasellas@gmail.com>
2021-02-11 19:19:23 -0700

    variant FreeCompose

--- modules/core/src/main/scala/com/adrielc/quivr/free/FreeCompose.scala
+++ modules/core/src/main/scala/com/adrielc/quivr/free/FreeCompose.scala
@@ -7,16 +7,18 @@ import cats.implicits._
 
 /**
  * Bare-bonez alternative to [[FreeArrow]]
-  */
-sealed abstract class FreeCompose[F[_, _], A, B] {
+ * - only supports [[compose]]/[[andThen]]
+ * - contravariant for [[A]] and covariant for [[B]]
+ */
+sealed abstract class FreeCompose[F[_, _], -A, +B] {
   import FreeCompose._
 
   /**
    * Stack safe if compose on [[G]] is stack safe
-    */
-  final def foldMap[G[_, _]](fg: F ~~> G)(implicit C: Compose[G]): G[A, B] = {
+   */
+  final def foldMap[G[-_, +_]](fg: F ~~> G)(implicit C: Compose[G]): G[A, B] = {
 
-    lazy val lazyAnd = new (FreeCompose[F, *, *] ~~> λ[(α, β) => Eval[G[α, β]]]) {
+    lazy val lazyAnd = new (FreeCompose[F, -*, +*] ~~> λ[(-[α], +[β]) => Eval[G[α, β]]]) {
       def apply[D, E](f: FreeCompose[F, D, E]): Eval[G[D, E]] = f match {
         case a: AndThen[F, D, b, E] =>
 
@@ -34,15 +36,13 @@ sealed abstract class FreeCompose[F[_, _], A, B] {
       case AndThen(AndThen(f, g), h) => (f >>> (g >>> h)).foldMap(fg)
 
       case andThen: AndThen[F, A, b, B] @unchecked => (for {
-          e <- lazyAnd(andThen.f)
-          b <- Eval.later(lazyAnd(andThen.g)).flatten
+        e <- lazyAnd(andThen.f)
+        b <- Eval.later(lazyAnd(andThen.g)).flatten
       } yield e >>> b).value
 
-      case LiftK(f) => fg(f)
+      case f: LiftK[F, A, B] => fg(f.f)
     }
   }
-
-  final def fold(implicit C: Compose[F]): F[A, B] = foldMap(BiFunctionK.id)
 
   def compose[Z](that: FreeCompose[F, Z, A]): FreeCompose[F, Z, B] =
     AndThen(that, this)
@@ -55,16 +55,10 @@ sealed abstract class FreeCompose[F[_, _], A, B] {
 
   def <<<[Z](that: FreeCompose[F, Z, A]): FreeCompose[F, Z, B] =
     this compose that
-
-  def >>^[C](f: F[B, C]): FreeCompose[F, A, C] =
-    AndThen(this, LiftK(f))
-
-  def <<^[C](f: F[C, A]): FreeCompose[F, C, B] =
-    AndThen(LiftK(f), this)
 }
 
 object FreeCompose {
-  def liftK[F[_, _], A, B](f: F[A, B]): FreeCompose[F, A, B] = LiftK(f)
+  def liftK[F[-_, +_], A, B](f: F[A, B]): FreeCompose[F, A, B] = LiftK(f)
 
   final private case class LiftK[F[_, _], A, B](f: F[A, B]) extends FreeCompose[F, A, B]
   final private case class AndThen[F[_, _], A, B, C](f: FreeCompose[F, A, B], g: FreeCompose[F, B, C]) extends FreeCompose[F, A, C]
